3d0163c03f386a55556b3657de00d18df7bc15b4
Benjamin Philippot <153378698+benPhilippot@users.noreply.github.com>
2025-09-25 14:01:11 +0200

    replace logic in OnDrawGizmos by [ExecuteInEditor]

--- Follow_Object.cs
+++ Follow_Object.cs
@@ -1,49 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+[ExecuteInEditMode]
 
 public class Follow_Bone : MonoBehaviour
 {
     [SerializeField] private GameObject parentObject;
     [SerializeField] private Vector3 Offset;
 
-    [SerializeField] private bool alwaysDrawDebugPosition = false;
     [SerializeField] private bool keepOrientation = true;
     [SerializeField] private bool keepScale = true;
+ 
+    [SerializeField] private bool alwaysDrawDebugPosition = false;
     
     private Vector3 _cubeSize = new Vector3(0.4f, 0.1f, 0.1f);
 
     void OnDrawGizmos()
     {
-        if (parentObject == null) 
+        if (parentObject == null)
             return;
 
-        if (UnityEditor.Selection.activeGameObject != this.gameObject && !alwaysDrawDebugPosition) 
+        if (UnityEditor.Selection.activeGameObject != this.gameObject && !alwaysDrawDebugPosition)
             return;
 
-        if (alwaysDrawDebugPosition) 
-            Gizmos.color = Color.yellow;
+        Gizmos.color = Color.yellow;
 
-        if (UnityEditor.Selection.activeGameObject == this.gameObject) 
+        if (UnityEditor.Selection.activeGameObject == this.gameObject)
             Gizmos.color = Color.green;
 
-        Vector3 worldPosition = parentObject.transform.TransformPoint(Offset);
+        Gizmos.DrawWireSphere(transform.position, 0.3f);
 
-        Gizmos.DrawWireSphere(worldPosition, 0.3f);
-        Gizmos.DrawLine(parentObject.transform.position, worldPosition);
+        Gizmos.DrawLine(parentObject.transform.position, transform.position);
 
         Matrix4x4 rotationMatrix = Matrix4x4.TRS(parentObject.transform.position, parentObject.transform.rotation, parentObject.transform.lossyScale);
         Gizmos.matrix = rotationMatrix;
         Gizmos.DrawWireCube(Vector3.zero, _cubeSize);
-
-        Quaternion worldRotation = parentObject.transform.rotation;
-        Vector3 worldScale = parentObject.transform.lossyScale;
-
-        transform.position = worldPosition;
-        if (!keepOrientation) 
-            transform.rotation = worldRotation;
-        if (!keepScale) 
-            transform.localScale = worldScale;
     }
 
 
@@ -63,4 +53,5 @@ public class Follow_Bone : MonoBehaviour
             transform.localScale = worldScale;
     }
 }
+
 
